f074f37b6a26985803f6a7672831e56b55c34f9e
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-05-31 07:51:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let take=(arr, n)=> arr.filter((x,i)=> i <= n-1 )
+
+
+/*
+
+8kyu- Enumerable Magic #25 - Take the First N Elements
+
+
+Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+If you need help, here's a reference:
